9f2f31d3b976fbd1e3543dab314e73f337ade56b
Jean <jeanc16rlos@gmail.com>
2024-05-30 21:47:08 +0200

    refactor(kali app): moved kali app into its own monorepo
    Wrapped mobile kali app into its own repo for the monorepo setup
    
    BREAKING CHANGE: kali now is a workspace

--- design-system/components/Layout/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as AspectRatio } from "./AspectRatio";
-export { default as Circle } from "./Circle";
-export { default as Center } from "./Center";
-export { default as HStack } from "./HStack";
-export { default as VStack } from "./VStack";
-export { default as ZStack } from "./ZStack";
-export { default as Spacer } from "./Spacer";
-export { default as Square } from "./Square";
--- design-system/core/index.tsx
+++
@@ -1,3 +0,0 @@
-export { withTheme, ThemeProvider } from "./theming";
-export { default as Provider } from "./Provider";
-export { default as DefaultTheme } from "./styles/DefaultTheme";
